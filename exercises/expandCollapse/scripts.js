239f53eeab03db3ea8edca119a287a7d567e4dbe
Nico Rithner <nicorithner@gmail.com>
2022-11-13 18:30:01 -0700

    Add alternative solution using className

--- exercises/expandCollapse/scripts.js
+++ exercises/expandCollapse/scripts.js
@@ -1,12 +1,22 @@
 let btn = document.getElementById("show-more");
 let content = document.getElementById("content");
 
-btn.addEventListener("click", () => {
-  if (btn.textContent === "Expand") {
-    content.style = "max-height: fit-content";
-    btn.textContent = "Collapse";
+// btn.addEventListener("click", () => {
+//   if (btn.textContent === "Expand") {
+//     content.style = "max-height: fit-content";
+//     btn.textContent = "Collapse";
+//   } else {
+//     content.style = "max-height: 30px";
+//     btn.textContent = "Expand";
+//   }
+// });
+
+btn.onclick = () => {
+  if (content.className == "") {
+    content.className = "open";
+    btn.innerHTML = "Collapse";
   } else {
-    content.style = "max-height: 30px";
-    btn.textContent = "Expand";
+    content.className = "";
+    btn.innerHTML = "Expand";
   }
-});
+};
