bac173b87b080cc93dbdbaebf7d3da1f6fbc5a50
Nico Rithner <nicorithner@gmail.com>
2022-11-18 22:28:28 -0700

    Start simple form exercise

---
+++ exercises/simpleForm/scripts.js
@@ -0,0 +1,9 @@
+let btn = document.querySelector("#button");
+
+btn.addEventListener("click", onButtonClick);
+
+function onButtonClick() {
+  let messageDiv = document.getElementById("message");
+  let name = document.getElementById("name").value;
+  let animal = document.getElementById("animal").value;
+}
