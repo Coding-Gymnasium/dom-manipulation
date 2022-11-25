d8f2643e2ab5161347c670f8294b7bf30a53520f
Nico Rithner <nicorithner@gmail.com>
2022-11-25 12:06:40 -0700

    Draw a dot when the mouse move inside square

--- exercises/draw/scripts.js
+++ exercises/draw/scripts.js
@@ -1,9 +1,13 @@
 let drawingBlock = document.querySelector(".drawingBlock");
 let message = document.querySelector("#message");
+let dotDiv = document.createElement("div");
+dotDiv.className = "dot";
 
 let count = 0;
-let displayText = () => {
+let OnMouseMove = () => {
   message.textContent += `${(count += 1)} `;
+
+  document.body.appendChild(dotDiv);
 };
 
-drawingBlock.addEventListener("mousemove", displayText);
+drawingBlock.addEventListener("mousemove", OnMouseMove);
