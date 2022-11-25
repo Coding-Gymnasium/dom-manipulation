0d8adf641442aceb87f278913b69605f61977291
Nico Rithner <nicorithner@gmail.com>
2022-11-25 12:23:00 -0700

    Dot tracks mouse position

--- exercises/draw/scripts.js
+++ exercises/draw/scripts.js
@@ -4,10 +4,14 @@ let dotDiv = document.createElement("div");
 dotDiv.className = "dot";
 
 let count = 0;
-let OnMouseMove = () => {
-  message.textContent += `${(count += 1)} `;
+let OnMouseMove = (e) => {
+  console.log(e.screenX, e.screenY);
+
+  // message.textContent += `${(count += 1)} `;
 
   document.body.appendChild(dotDiv);
+  dotDiv.style.top = `${e.y}px`;
+  dotDiv.style.left = `${e.x}px`;
 };
 
 drawingBlock.addEventListener("mousemove", OnMouseMove);
