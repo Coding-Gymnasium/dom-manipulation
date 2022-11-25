226d31bd65f70fa41d3b9c9e44767f1b408f1219
Nico Rithner <nicorithner@gmail.com>
2022-11-25 12:29:07 -0700

    Add drawing a line with the dot as mouse moves

--- exercises/draw/scripts.js
+++ exercises/draw/scripts.js
@@ -1,17 +1,15 @@
 let drawingBlock = document.querySelector(".drawingBlock");
 let message = document.querySelector("#message");
-let dotDiv = document.createElement("div");
-dotDiv.className = "dot";
 
 let count = 0;
 let OnMouseMove = (e) => {
-  console.log(e.screenX, e.screenY);
-
   // message.textContent += `${(count += 1)} `;
 
+  let dotDiv = document.createElement("div");
+  dotDiv.className = "dot";
+  dotDiv.style.top = `${e.y + 5}px`;
+  dotDiv.style.left = `${e.x - 5}px`;
   document.body.appendChild(dotDiv);
-  dotDiv.style.top = `${e.y}px`;
-  dotDiv.style.left = `${e.x}px`;
 };
 
 drawingBlock.addEventListener("mousemove", OnMouseMove);
