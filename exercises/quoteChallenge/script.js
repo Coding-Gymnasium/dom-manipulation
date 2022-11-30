b90692dfba93120fad8ec3aff0cd5b32e99b8ab9
Nico Rithner <nicorithner@gmail.com>
2022-11-30 15:58:45 -0700

    Add conditional so it only closes when pressing x

--- exercises/quoteChallenge/script.js
+++ exercises/quoteChallenge/script.js
@@ -3,5 +3,10 @@ let container = document.querySelector(".container");
 container.addEventListener("click", closeQuoteBox);
 
 function closeQuoteBox(e) {
-  e.target.parentElement.classList.add('remove');
+  let target = e.target;
+  if (target.className != "close") {
+    return;
+  } else {
+    target.parentElement.classList.add("remove");
+  }
 }
