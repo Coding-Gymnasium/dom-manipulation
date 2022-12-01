858a33f8230e0fabdb6bf42b6ded28381643a05a
Nico Rithner <nicorithner@gmail.com>
2022-12-01 12:34:55 -0700

    Refactor to toggle class 'display'

--- exercises/listCollapseExpandChallenge/script.js
+++ exercises/listCollapseExpandChallenge/script.js
@@ -5,13 +5,6 @@ foodList.addEventListener("click", collapseBelowList);
 function collapseBelowList(e) {
   let target = e.target;
   for (const item of target.children) {
-    switch (true) {
-      case item.tagName === "UL" && item.style.display === "block":
-        item.style.display = "none";
-        break;
-      default:
-        item.style.display = "block";
-        break;
-    }
+    item.classList.toggle("display");
   }
 }
