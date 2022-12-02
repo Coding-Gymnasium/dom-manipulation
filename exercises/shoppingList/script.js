34cd23aba15012f26cafea5c109e075ed7bda7ef
Nico Rithner <nicorithner@gmail.com>
2022-12-02 15:32:24 -0700

    Add logic to add item. Clean up css file

---
+++ exercises/shoppingList/script.js
@@ -0,0 +1,35 @@
+// add an item to the list
+const addItemForm = document.querySelector(".add-bar");
+const list = document.querySelector(".list");
+
+addItemForm.addEventListener("submit", handleAddItem);
+
+function handleAddItem(e) {
+  e.preventDefault();
+
+  // get data
+  const formData = new FormData(addItemForm);
+  console.log(formData.get("add-bar"));
+
+  // Add item to list
+  let inputFieldValue = formData.get("add-bar");
+
+  buildItemElement(inputFieldValue);
+
+  // clear field
+  document.querySelector("input[name='add-bar']").value = "";
+}
+
+function buildItemElement(data) {
+  let input = parseInput(data);
+  list.insertAdjacentHTML(
+    "afterbegin",
+    `<div id="list-item"><p>${input}</p><button type="submit">delete</button></div>`
+  );
+}
+
+function parseInput(data) {
+  let withoutFirst = data.slice(1).toLowerCase();
+  let firstChar = data.slice(0, 1).toUpperCase();
+  return firstChar + withoutFirst;
+}
